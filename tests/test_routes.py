40333910e5944944dbbf94b6b1018dfbd10d0df4
jaogye <jao_ec@yahoo.com>
2024-02-08 07:35:19 -0500

    added nose arguments

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,22 +135,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-    ######################################################################
-    # READ AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
-        app.logger.info("Request to read an Account with id: %s", account_id)
-
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-        return account.serialize(), status.HTTP_200_OK
 
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
@@ -193,4 +177,3 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)        
 
-        
